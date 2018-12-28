2265d387c204cf5ba2f4ddb3bfd9dbaf0775e168
itzik <itzikd15@gmail.com>
2018-12-29 00:46:01 +0200

    Part2

--- src/Model/ReadQuery.java
+++ src/Model/ReadQuery.java
@@ -32,19 +32,29 @@ public class ReadQuery {
         String ParseMe = "";
         int i = 0;
         String queryNum = "";
+        String queryDesc = "";
         while ((st = br.readLine()) != null) {
             if (st.equals("<top>")) {
                 QueryTitle = new StringBuilder();
             } else if (st.startsWith("<title>")) {
                 ParseMe = st;
             } else if (st.equals("</top>")) {
-                Query q = new Query(queryNum, ParseMe.substring(8), "", "");
+                Query q = new Query(queryNum, ParseMe.substring(8), queryDesc, "");
                 q = pq.parse(q);
                 queries.add(q);
             } else if (st.startsWith("<num> ")) {
                 String[] words = st.split(" ");
                 queryNum = words[words.length - 1];
+            } else if (st.startsWith("<desc>")) {
+                String desc = "";
+                st = br.readLine();
+                while (!st.startsWith("<narr>")) {
+                    desc += st;
+                    st = br.readLine();
+                }
+                queryDesc = desc;
             }
+
         }
         return queries;
     }
